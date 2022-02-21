4a2c0fa33c448574d6675fc8fa5430726c1f86a7
AngelicaJH <isc.florjardinez@gmail.com>
2022-02-21 13:50:24 -0600

    primer observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
+import { Observable, Subscriber } from "rxjs";
 
+//crear obser.
+// const obs$=Observable.create();
+const obs$ = new Observable<string>(subs => { //puede ir sin <string, pero se recomienda indicar con que tipo de datos trabajara
+    //para que un subscriber se ejecuta debe tener una subcripcion
+    subs.next('Hola')
+    subs.next('hola')
 
-console.log('Hola Mundo!');
+    subs.complete();//se completa
 
+    subs.next('que hace?')//ya no se mostrara
 
+})
 
-
-
-
-
+obs$.subscribe(console.log)
